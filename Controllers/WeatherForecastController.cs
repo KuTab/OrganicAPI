8bbf6e2bbed0f509c247707bd53a5edc8a8782bd
Egor Dadugin <egor140202@yandex.ru>
2023-04-18 17:24:27 +0300

    authController init

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace OrganicAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/weather")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -18,7 +18,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet("getForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
